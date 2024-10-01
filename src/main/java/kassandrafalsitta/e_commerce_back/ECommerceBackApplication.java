1d847b1a0aea662fd7a6acbe1002eb68b07a6e8a
kassandra falsitta <160047671+kassandra2000@users.noreply.github.com>
2024-10-01 17:58:48 +0200

    create repository

---
+++ src/main/java/kassandrafalsitta/e_commerce_back/ECommerceBackApplication.java
@@ -0,0 +1,13 @@
+package kassandrafalsitta.e_commerce_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceBackApplication.class, args);
+	}
+
+}
